49c5d63e313d4e41ea043720613429c4760ca730
Shiung <hh.shiung@gmail.com>
2018-08-27 09:21:32 +0800

    working version 01

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,50 +1,119 @@
 <template>
-  <div class="calculator">
-    <div class="display">{{ displayValue }}</div>
-    
-    <div type="button" class="btn" @click="clear()">C</div>
-    <div type="button" class="btn" @click="changeSign()">+/-</div>
-    <div type="button" class="btn" @click="percent()">%</div>
-    <div type="button" class="btn operator">÷</div>
-    <div type="button" class="btn">7</div>
-    <div type="button" class="btn">8</div>
-    <div type="button" class="btn">9</div>
-    <div type="button" class="btn operator">*</div>
-    <div type="button" class="btn">4</div>
-    <div type="button" class="btn">5</div>
-    <div type="button" class="btn">6</div>
-    <div type="button" class="btn operator">-</div>
-    <div type="button" class="btn">1</div>
-    <div type="button" class="btn">2</div>
-    <div type="button" class="btn">3</div>
-    <div type="button" class="btn operator">+</div>
-    <div type="button" class="btn" style="grid-column: 1 / 3" >0</div>
-    <div type="button" class="btn">.</div>
-    <div type="button" class="btn operator">=</div>
+  <div>
+    <div class="calculator">
+      <div class="display">{{ displayText || 0 }}</div>
+
+      <div type="button" class="btn" @click="clear()">C</div>
+      <div type="button" class="btn" @click="changeSign()">+/-</div>
+      <div type="button" class="btn" @click="percent()">%</div>
+      <div type="button" class="btn operator" @click="divide()">÷</div>
+      <div type="button" class="btn" @click="append('7')">7</div>
+      <div type="button" class="btn" @click="append('8')">8</div>
+      <div type="button" class="btn" @click="append('9')">9</div>
+      <div type="button" class="btn operator" @click="multiply()">*</div>
+      <div type="button" class="btn" @click="append('4')">4</div>
+      <div type="button" class="btn" @click="append('5')">5</div>
+      <div type="button" class="btn" @click="append('6')">6</div>
+      <div type="button" class="btn operator" @click="subtract()">-</div>
+      <div type="button" class="btn" @click="append('1')">1</div>
+      <div type="button" class="btn" @click="append('2')">2</div>
+      <div type="button" class="btn" @click="append('3')">3</div>
+      <div type="button" class="btn operator" @click="add()">+</div>
+      <div type="button" class="btn" style="grid-column: 1 / 3" >0</div>
+      <div type="button" class="btn" @click="appendDot()">.</div>
+      <div type="button" class="btn operator" @click="equal()">=</div>
+    </div>
+
+    <div>
+      <span>
+        Current value: {{ this.$data.displayText }}<br />
+        Previous value: {{ this.$data.previous }}
+      </span>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Calculator',
-  
+
   data () {
     return {
-      displayValue: 10
+      displayText: null,
+      previous: null, // Holds paramter a for operator()
+      operator: null, // To hold callback functions for operators
+
+      operatorClicked: false
     }
   },
 
   methods: {
     clear () {
-      this.$data.displayValue = 0
+      this.$data.displayText = ''
     },
 
     changeSign () {
-      this.$data.displayValue *= -1
+      if (this.$data.displayText) {
+        this.$data.displayText = this.$data.displayText.charAt(0) === '-'
+          ? this.$data.displayText.substr(1)
+          : `-${this.$data.displayText}`
+      } // else: Do nothing
     },
 
     percent () {
-      this.$data.displayValue /= 100
+      this.$data.displayText = `${parseFloat(this.$data.displayText / 100)}`
+    },
+
+    append (number) {
+      if (this.$data.operatorClicked) {
+        this.clear()
+      } // else: Do nothing
+
+      this.$data.displayText = this.$data.displayText === null ? `${number}` : `${this.$data.displayText}${number}`
+    },
+
+    appendDot () {
+      if (this.$data.displayText.indexOf('.') === -1) {
+        this.append('.')
+      } // else: Do nothing
+    },
+
+    setPrevious () {
+      this.$data.previous = this.$data.displayText
+      this.$data.operatorClicked = true // THis may be useful for something else...
+    },
+
+    divide () {
+      this.operator = (a, b) => a / b
+      this.setPrevious()
+    },
+
+    multiply () {
+      this.operator = (a, b) => a * b
+      this.setPrevious()
+    },
+
+    subtract () {
+      this.operator = (a, b) => a - b
+      this.setPrevious()
+    },
+
+    add () {
+      this.operator = (a, b) => a + b
+      this.setPrevious()
+    },
+
+    equal () {
+      this.$data.displayText = `${this.$data.operator(parseFloat(this.$data.previous), parseFloat(this.$data.displayText))}`
+      this.$data.previous = null
+      this.$data.operatorClicked = false
+    }
+  },
+
+  filter: {
+    // String -> Number
+    toNumber(payload) {
+      return Number(payload)
     }
   }
 }
